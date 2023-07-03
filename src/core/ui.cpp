7232d686f58556248c30e0a9aeaeec49e257c87f
Yuhki <yuhkixx@protonmail.com>
2023-07-03 23:57:52 +0200

    added fonts and adjusted design

--- src/core/ui.cpp
+++ src/core/ui.cpp
@@ -2,10 +2,19 @@
 
 void UI::Render()
 {
-	ImGui::SetNextWindowSize(startup_size);
-	ImGui::Begin("ColorSnap");
+	ImGui::SetNextWindowSize(startup_size, ImGuiCond_Always);
+	ImGui::Begin("ColorSnap", nullptr, flags);
 	{
+		ImGui::SetCursorPos(ImVec2(0, 104));
+		ImGui::BeginChild("color list", child_size);
+		{
+			ImGui::BeginChild("colors");
+			{
 
+			}
+			ImGui::EndChild();
+		}
+		ImGui::EndChild();
 	}
 	ImGui::End();
 }
--- src/core/ui.h
+++ src/core/ui.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <imgui/imgui.h>
 #include <imgui/imgui_internal.h>
+#include <wtypes.h>
+#include "../fonts/InterRegular.h"
+#include "../fonts/InterSemiBold.h"
 
 class UI
 {
@@ -14,10 +17,12 @@ public:
 		auto colors = style.Colors;
 
 		// Colors
-		colors[ImGuiCol_WindowBg] = ImColor(255, 255, 255, 255);
-		colors[ImGuiCol_ChildBg] = ImColor(255, 255, 255, 255);
-
+		colors[ImGuiCol_WindowBg] = ImColor(9, 14, 20);
+		colors[ImGuiCol_ChildBg] = ImColor(12, 22, 34);
+		
 		// Others styles
+		style.WindowPadding = ImVec2(0, 0);
+		style.WindowBorderSize = 0.f;
 
 		
 	}
@@ -26,9 +31,15 @@ public:
 	{
 		ImGuiIO& io = ImGui::GetIO();
 		auto fonts = io.Fonts;
+		InterSemiBold = fonts->AddFontFromMemoryCompressedTTF(InterSemiBold_compressed_data, InterSemiBold_compressed_size, 12.f);
+		InterRegular = fonts->AddFontFromMemoryCompressedTTF(InterRegular_compressed_data, InterRegular_compressed_size, 12.f);
 	}
 
 private:
-	ImVec2 startup_size = ImVec2(500, 500);
+	ImFont* InterSemiBold = nullptr;
+	ImFont* InterRegular = nullptr;
+	ImVec2 startup_size = ImVec2(410, 323);
+	ImVec2 child_size = ImVec2(410, 219);
+	DWORD flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize;
 };
 
